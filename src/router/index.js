954d663e700496cc2b199323b4fea28c832f5eec
akrem jomaa <jomaakrem@gmail.com>
2023-03-29 15:30:37 +0200

    adding creating account

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: () => import('../views/HomeView.vue')
+    },
+    {
+      path: '/createAccount',
+      name: 'createAccount',
+      component: () => import('../views/CreateUserAccountView.vue'),
     }
   ]
 })
