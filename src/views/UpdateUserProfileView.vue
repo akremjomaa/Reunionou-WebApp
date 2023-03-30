4a084076db9e57063d622bd023850599d9e6de19
WZidane <wail.zidane@hotmail.com>
2023-03-30 16:25:36 +0200

    Update User Profile

--- src/views/UpdateUserProfileView.vue
+++ src/views/UpdateUserProfileView.vue
@@ -20,64 +20,70 @@ onMounted(() => {
     getUser();
 });
 
-async function putUser(){
-    await axios.put(`https://api.reunionou.local:19043/users/${route.params.id}`, {body: member}).then(response =>{
+async function putUser() {
+    await axios.put(`https://api.reunionou.local:19043/users/${route.params.id}`, {
+        name: member.lastname,
+        firstname: member.firstname,
+        email: member.email,
+        password: member.password
+    }).then(response => {
         console.log(response);
     }).catch(error => console.log(error));
 
     await router.push(`/user/${member.id}`);
 }
-async function getUser(){
-    await axios.get(`https://api.reunionou.local:19043/users/${route.params.id}`).then(response =>{
-      console.log(response.data.user);
-      member.id = response.data.user.id;
-      member.lastname = response.data.user.lastname;
-      member.firstname = response.data.user.firstname;
-      member.email = response.data.user.email;
-      member.password = response.data.user.password;
-      member.status = response.data.user.status;
+
+async function getUser() {
+    await axios.get(`https://api.reunionou.local:19043/users/${route.params.id}`).then(response => {
+        console.log(response.data.user);
+        member.id = response.data.user.id;
+        member.lastname = response.data.user.lastname;
+        member.firstname = response.data.user.firstname;
+        member.email = response.data.user.email;
+        member.password = response.data.user.password;
+        member.status = response.data.user.status;
     })
 }
 </script>
 <template>
-  <main>
-    <form @submit.prevent="putUser">
+    <main>
+        <form @submit.prevent="putUser">
 
-        <div>
-            <label>Prénom</label>
             <div>
-                <input class="input" v-model="member.firstname" required="required" placeholder="Prénom">
+                <label>Prénom</label>
+                <div>
+                    <input class="input" v-model="member.firstname" required="required" placeholder="Prénom">
+                </div>
             </div>
-        </div>
 
-        <div>
-            <label>Nom</label>
             <div>
-                <input class="input" v-model="member.lastname" required="required" placeholder="Nom">
+                <label>Nom</label>
+                <div>
+                    <input class="input" v-model="member.lastname" required="required" placeholder="Nom">
+                </div>
             </div>
-        </div>
 
-        <div>
-            <label>E-mail</label>
             <div>
-                <input class="input" v-model="member.email" required="required" type="email" placeholder="E-mail">
+                <label>E-mail</label>
+                <div>
+                    <input class="input" v-model="member.email" required="required" type="email" placeholder="E-mail">
+                </div>
             </div>
-        </div>
 
-        <div>
-            <label>Mot de Passe</label>
             <div>
-                <input v-model="member.password" required="required" type="password" placeholder="Mot de Passe">
+                <label>Mot de Passe</label>
+                <div>
+                    <input v-model="member.password" required="required" type="password" placeholder="Mot de Passe">
+                </div>
             </div>
-        </div>
 
-        <div>
             <div>
-                <button type="submit">Mettre à jour vos informations ?</button>
+                <div>
+                    <button type="submit">Mettre à jour vos informations ?</button>
+                </div>
             </div>
-        </div>
-    </form>
-  </main>
+        </form>
+    </main>
 
 </template>
 <style scoped>
