90ca041e5e1f186c94abe22f4224dd4cb7cfab7a
WZidane <wail.zidane@hotmail.com>
2023-03-30 12:34:46 +0200

    Initialization V3

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: () => import('../views/HomeView.vue')
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: () => import('../views/SignUpView.vue')
     }
   ]
 })
---
+++ src/views/SignUpView.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <h1>Sign Up</h1>
+  </main>
+</template>
