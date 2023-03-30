8550379c1b5c0a1f146c0ca14dc748988916ff28
leosix10 <leo.six1@etu.univ-lorraine.fr>
2023-03-30 12:03:45 +0200

    init Navbar CSS

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,17 +3,59 @@
 </script>
 
 <template>
-  <nav v-if="$route.path !== '/DisplayDevice'" class="navbar" role="navigation" aria-label="main navigation">
+  <nav v-if="$route.path !== '/DisplayDevice'" class="navbar pl-4 pr-4" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
       <router-link to="/" class="navbar-item">
-        <a href="#">Link</a>
+        <img src="/logo.png" alt="logo" width="35" height="35">
       </router-link>
+      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
     </div>
-      <div class="navbar-menu">
+
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start">
+        <router-link to="/" class="navbar-item">
+          Mes évènements
+        </router-link>
+        <router-link to="/" class="navbar-item">
+          Mes invitations
+        </router-link>
       </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <!--Navbar utilisateur connecté-->
+          <div class="buttons">
+            <router-link to="/">
+              <span class="icon is-large">
+                <i class="fas fa-2x fa-user"></i>
+              </span>
+            </router-link>
+            <router-link to="/">
+              <span class="icon is-large">
+                <i class="fas fa-2x fa-sign-out-alt"></i>
+              </span>
+            </router-link>
+          </div>
+          
+          <!--Navbar utilisateur déconnecté-->
+
+          <div class="buttons">
+            <router-link to="/" class="button is-primary">
+              <strong>S'inscrire</strong>
+            </router-link>
+            <router-link to="/" class="button is-light">
+              Se connecter
+            </router-link>
+          </div>
+        </div>
+      </div>
+
+    </div>
   </nav>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
