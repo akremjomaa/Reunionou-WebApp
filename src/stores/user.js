4f3584b2f34f31681c3531df966dbb9ff5b54d22
KhaoulaCode <boulhdir.khaoula@gmail.com>
2023-04-06 10:22:20 +0200

    adding stores

---
+++ src/stores/user.js
@@ -0,0 +1,26 @@
+import { reactive } from "vue";
+import { defineStore } from "pinia";
+import { useRouter } from "vue-router";
+
+export const useUserStore = defineStore('user', () => {
+    const state = reactive({
+        TOKEN: '',
+        USER: ''
+    });
+
+    const router = useRouter();
+
+    function connexion() {
+        router.push('/connexion');
+    }
+
+    function sedeconnecter() {
+        state.TOKEN = '';
+        state.USER = '';
+        router.push('/connexion');
+    }
+
+    return {state, connexion, sedeconnecter};
+}, {
+    persist: true,
+})
--- src/views/ConnexionView.vue
+++ src/views/ConnexionView.vue
@@ -1,20 +1,39 @@
 <script setup>
+import axios from 'axios';
+import { useUserStore } from '../stores/user';
+import { BASE } from '../../public/config';
+import { reactive } from 'vue';
+
+
+const user = useUserStore();
+
+const state = reactive({
+    email: '',
+    password: ''
+})
+
+async function connect() {
+    await axios.post(`http://api.auth.local:19780/v1/signin`, { email: state.email, password: state.password }).then((response) => {
+        console.log(response);
+    })
+}
+
 </script>
 
 <template>
     <section class="column pt-6 is-10-tablet is-offset-1-tablet is-8-desktop is-offset-2-desktop is-6-widescreen is-offset-3-widescreen is-4-fullhd is-offset-4-fullhd">
         <h1 class="title is-2 has-text-centered">Connexion</h1>
-        <form action="" class="box">
+        <form class="box" @submit.prevent="connect">
             <div class="field">
                 <label class="label">E-mail</label>
                 <div class="control">
-                    <input class="input" required="required" placeholder="Votre e-mail">
+                    <input class="input" required="required" v-model="state.email" type="email" placeholder="Votre e-mail">
                 </div>
             </div>
             <div class="field">
                 <label class="label">Mot de passe</label>
                 <div class="control">
-                    <input class="input" required="required" placeholder="Votre mot de passe">
+                    <input class="input" required="required" v-model="state.password" type="password" placeholder="Votre mot de passe">
                 </div>
             </div>
 
